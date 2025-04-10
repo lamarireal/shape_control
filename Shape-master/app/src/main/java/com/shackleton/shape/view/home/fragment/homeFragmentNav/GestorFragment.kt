a57ef60dbca68d431a18f05d37459862ecad2807
ximenaqz <jimenaquiroz.1510@gmail.com>
2025-04-10 12:35:35 +0200

    version-3
    dIseño terminado me falta coneccion la base de datos

--- Shape-master/app/src/main/java/com/shackleton/shape/view/home/fragment/homeFragmentNav/GestorFragment.kt
+++ Shape-master/app/src/main/java/com/shackleton/shape/view/home/fragment/homeFragmentNav/GestorFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.shackleton.shape.custom.adapter.ModuleAdapter
 import com.shackleton.shape.databinding.FragmentGestorBinding
+import com.shackleton.shape.view.home.MainHome
 import com.shackleton.shape.view.home.ModuleViewModel
 
 class GestorFragment : Fragment() {
@@ -25,6 +27,24 @@ class GestorFragment : Fragment() {
     ): View {
         _binding = FragmentGestorBinding.inflate(inflater, container, false)
 
+        // Oculta la bottom navigation cuando entras
+        (activity as? MainHome)?.setBottomNavigationVisibility(View.GONE)
+
+
+        // el nav
+        (activity as? MainHome)?.hideToolbar()
+
+
+
+
+
+        setupRecycler()
+        setupBackButton()
+
+        return binding.root
+    }
+
+    private fun setupRecycler() {
         val ocultos = viewModel.getHiddenModules().toMutableList()
 
         adapter = ModuleAdapter(
@@ -38,14 +58,24 @@ class GestorFragment : Fragment() {
             }
         )
 
-        binding.recyclerGestor.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerGestor.layoutManager = GridLayoutManager(requireContext(), 1)
         binding.recyclerGestor.adapter = adapter
+    }
 
-        return binding.root
+    private fun setupBackButton() {
+        binding.arrowBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        // Vuelve a mostrar la bottom navigation al salir
+        (activity as? MainHome)?.setBottomNavigationVisibility(View.VISIBLE)
+        (activity as? MainHome)?.showToolbar()
+
+
         _binding = null
     }
 }
